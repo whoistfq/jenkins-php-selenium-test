f47e177b2266149e02cc56db30444ee0c7b450b5
whoistfq <muhd62@gmail.com>
2024-07-21 22:13:21 +0800

    d

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://192.168.18.85";
+	String url = "http://172.17.0.4";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
